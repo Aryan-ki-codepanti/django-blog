71aceac97f302aeed049cc00bfd81611c1da290b
Aryan Sethi <aryan.rdps@gmail.com>
2021-10-29 11:22:10 +0530

    Added blog comment model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.expressions import F
+from django.utils.timezone import now
 
 # Create your models here.
 class Post(models.Model):
@@ -11,3 +14,15 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+class BlogComment(models.Model):
+    sno = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    post = models.ForeignKey(Post , on_delete=models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    parent = models.ForeignKey('self' , on_delete=models.CASCADE , null=True)
+    timeStamp = models.DateTimeField(default=now)
+    # parent
+
+    def __str__(self):
+        return f"{self.comment} by {self.user.username} on {self.post.title[0:20]}..."
--- iCoder/urls.py
+++ iCoder/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "iCoder Admin"
+admin.site.site_title = "Welcome Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
